2a680ef9047d616e576a5c6e7ef39b7d2cf6f6c5
Enar Mariinsky <enar.mariinsky@gmail.com>
2019-12-02 11:53:29 +0200

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 const express = require('express');
 const app  = express();
+const cors = require('cors');
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
