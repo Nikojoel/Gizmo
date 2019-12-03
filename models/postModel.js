126512723544d78c5f07fa3db50dfb464293067e
Enar Mariinsky <enar.mariinsky@gmail.com>
2019-12-03 08:15:23 +0200

    get all posts updated sql query to correctly count number of comments

--- models/postModel.js
+++ models/postModel.js
@@ -5,13 +5,13 @@ const promisePool = pool.promise();
 const getAllPosts = async () => {
     try {
         const [rows] = await promisePool.execute(
-            'SELECT post_title, post_file, COUNT(comment_post_id) as count_comments, COUNT(vote_post_id) as count_vote, ' +
+            'SELECT post_title, post_file, ' +
+            '(SELECT COUNT(*) FROM comment WHERE comment_post_id = post_id) AS count_comments, ' +
+            '(SELECT COUNT(*) FROM vote WHERE vote_post_id = post_id) AS count_vote, ' +
             'user_name , user_id , user_picture ' +
             'FROM post JOIN user ON ' +
-            'post_owner = user_id ' +
-            'JOIN comment ON post_id = comment_post_id ' +
-            'JOIN vote ON post_id = vote_post_id ' +
-            'group by comment_post_id;'
+            'post_owner = user_id '
+
 
             );
         return rows;
