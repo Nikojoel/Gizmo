63fb776f577ddbab0b4158ea2687672e6739180d
Enar Mariinsky <enar.mariinsky@gmail.com>
2019-12-07 14:26:20 +0200

    css

---
+++ utils/multer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const imageFilter = function(req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+exports.imageFilter = imageFilter;
