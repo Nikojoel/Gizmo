8be44f0744de8e0b8f3b310fe7730090280cf74d
Enar Mariinsky <enar.mariinsky@gmail.com>
2019-12-08 11:34:54 +0200

    added docs folder and some imaged for readme

--- public/js/serialize.js
+++ public/js/serialize.js
@@ -4,7 +4,7 @@
  Copyright (c) 2017 yiming.hsu <mufasa.hsu@gmail.com}
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
+ of this software and associated docs files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
